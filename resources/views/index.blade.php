0fe7c89dba960e976a07260dd07902f14b69e102
Noel Calonia <emo_noel10@yahoo.com>
2020-01-23 15:45:17 +0800

    Update index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet">
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="/js/app.js" defer></script>
     </head>
     <body>
         <div id="app"></div>
